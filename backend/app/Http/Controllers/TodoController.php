0dfc9d3c93a959828bc345f2cbf1d66837f12731
mohammad <mysaltern@gmail.com>
2025-02-05 17:49:15 +1100

    store added

--- backend/app/Http/Controllers/TodoController.php
+++ backend/app/Http/Controllers/TodoController.php
@@ -2,18 +2,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class TodoController extends Controller
 {
-    private $todos = [];
-
-    public function __construct()
-    {
-        session()->put('todo', session('todos', []));
-    }
+    private $cacheKey = 'todos';
 
     public function index()
     {
-        return response()->json(session('todos'));
+        $todos = Cache::get($this->cacheKey, []);
+        return response()->json($todos);
     }
+
+    public function store(Request $request)
+    {
+        $title = $request->input('title');
+
+        if (!$title) {
+            return response()->json(['error' => 'Title is required'], 400);
+        }
+
+        $newTodo = [
+            'id' => uniqid(),
+            'title' => $title,
+            'completed' => false,
+        ];
+
+        $todos = Cache::get($this->cacheKey, []);
+        $todos[] = $newTodo;
+
+        Cache::put($this->cacheKey, $todos);
+
+        return response()->json($newTodo);
+    }
+
+ 
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,3 +3,4 @@
 use App\Http\Controllers\TodoController;
 
 Route::get('/todo', [TodoController::class, 'index']);
+Route::post('/todo/store', [TodoController::class, 'store']);
